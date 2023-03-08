8f16c2ac136b8eec7c021dc01bd662cd4646135f
Maria T. Andersen <mteilmann93@gmail.com>
2023-03-08 18:45:26 +0100

    Add commentary to current lines of code which explains why the lines were written and what they do to further the Express application. Make the application listen to port 9000 and print a console.log message to the user once connection has been established.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const port = 9000;
 
+/**
+ * Create Express application by calling the Express function.
+ * The Express function returns an object which we want to store into a variable constant for later use
+ */
 const app = express();
+
+/**
+ * We have created an Express application.
+ * Now we can call a method of app called listen and make the Express application listen to a specific port and execute a specific line of code once a user connects.
+ * Let us print a console.log message in our case.
+ */
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
